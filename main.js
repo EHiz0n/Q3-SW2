428c288ad5ed4a84cf9adc69c5c9fb7776b2db84
EHiz0n <hizon.e-@obmontessori.edu.ph>
2025-03-25 07:19:40 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+        function product() {
+            var A value = document.getElementById("A value").value;
+            var B value = document.getElementById("B value").value;
+            window.alert("Product of A and B: " + (a * b));
+        }
+
+        function remainder() {
+            var A value = document.getElementById("A value").value;
+            var B value = document.getElementById("B value").value;
+            window.alert("Remainder when A is divided by B: " + (a % b));
+        }
+
+        function power() {
+            var A value = document.getElementById("A value").value;
+            var B value = document.getElementById("B value").value;
+            window.alert("A raised to the power of B: " + (a ** b));
+        }
+
+        function checkEquality() {
+            var A value = document.getElementById("A value").value;
+            var B value = document.getElementById("B value").value;
+            window.alert(a == b ? "A is equal to B" : "A is NOT equal to B");
+        }
